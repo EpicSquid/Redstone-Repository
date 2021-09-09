b6160a6df62a38df13849713e632428dcead8a10
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-09-09 11:05:01 +0100

    added test items

--- src/main/java/thundr/redstonerepository/handlers/RegistryHandler.java
+++ src/main/java/thundr/redstonerepository/handlers/RegistryHandler.java
@@ -1,10 +1,33 @@
 package thundr.redstonerepository.handlers;
 
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.inventory.container.ContainerType;
+import net.minecraft.item.Item;
+import net.minecraft.item.crafting.IRecipeSerializer;
+import net.minecraft.particles.ParticleType;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
 import thundr.redstonerepository.RedstoneRepository;
 
 public class RegistryHandler {
 
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, RedstoneRepository.MODID);
+    public static final DeferredRegister<Enchantment> ENCHANTMENTS = DeferredRegister.create(ForgeRegistries.ENCHANTMENTS, RedstoneRepository.MODID);
+    public static final DeferredRegister<IRecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, RedstoneRepository.MODID);
+    public static final DeferredRegister<ParticleType<?>> PARTICLE_TYPES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, RedstoneRepository.MODID);
+    public static final DeferredRegister<ContainerType<?>> CONTAINERS = DeferredRegister.create(ForgeRegistries.CONTAINERS, RedstoneRepository.MODID);
+
     public static void init() {
-        RedstoneRepository.LOGGER.info("registry init");
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ENCHANTMENTS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        RECIPE_SERIALIZERS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        PARTICLE_TYPES.register(FMLJavaModLoadingContext.get().getModEventBus());
+        CONTAINERS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
+
+    // Items:
+    public static final RegistryObject<Item> TEST = ITEMS.register("test", () -> new Item(new Item.Properties().tab(RedstoneRepository.tabRedstoneRepository)));
+    public static final RegistryObject<Item> TEST_TWO = ITEMS.register("test_two", () -> new Item(new Item.Properties().tab(RedstoneRepository.tabRedstoneRepository)));
 }
